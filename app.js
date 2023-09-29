ebe3d6a32b847ea9d863b1531ae7d06e7b356f23
Jorge Saldana <jojonoob134@gmail.com>
2023-09-29 15:01:20 -0500

    did the thing

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+lastName = "saldana"
+age = 18;
+const firstName = "Jorge";
 // Exercise 3
+let language = "javaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//firstName = "jojo"
+// Exercise 4
+let price = 19.99;
+let isOneSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wild";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+console.log(`${firstName} ${age} ${lastName}`)
+console.log(`${language} ${createdYear} ${isCaseSensitive}`)
+console.log(`${price} ${isOneSale} ${salePercentage} ${stock} ${inStock} ${selectedSize}`)
+console.log(`${title} ${author} ${pageCount} ${bookmark} ${hasRead}`)
